e65d38d2b452806a771bbfcc3d2f5f732b3b3c65
Pooja Pandey <09Pooja99@gmail.com>
2025-02-05 19:46:45 +0530

    updated EDA

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -48,6 +48,7 @@ DATA_TRANSFORMATION_PREPROCESSED_FILE_NAME_KEY = "preprocessed_object_file_name"
 
 
 
+
 COLUMN_TOTAL_ROOMS = "total_rooms"
 COLUMN_POPULATION = "population"
 COLUMN_HOUSEHOLDS = "households"
