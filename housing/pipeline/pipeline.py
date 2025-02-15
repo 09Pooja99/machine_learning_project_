e02ee8b253faeb8d28d0ed694fbe6c2e5f3ad000
Pooja Pandey <09Pooja99@gmail.com>
2025-02-15 11:05:19 +0530

    model trained

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,46 +1,65 @@
-from housing.config.configuration import Configuartion
-from housing.logger import logging
+from collections import namedtuple
+from datetime import datetime
+import uuid
+from housing.config.configuration import Configuration
+from housing.logger import logging,get_log_file_name
 from housing.exception import HousingException
+from threading import Thread
+from typing import List
+from multiprocessing import Process
 
-from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
+from housing.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
 from housing.entity.config_entity import DataIngestionConfig
 from housing.component.data_ingestion import DataIngestion
 from housing.component.data_validation import DataValidation
 from housing.component.data_transformation import DataTransformation
-import os,sys
+from housing.component.model_trainer import ModelTrainer
 
-class Pipeline:
+import os, sys
+from collections import namedtuple
+from datetime import datetime
+import pandas  as pd
+from housing.constant import EXPERIMENT_DIR_NAME
+Experiment = namedtuple("Experiment",["experiment_id","initialization_timestamp","log_file_name",
+"running_status","start_time","stop_time","execution_time","message","experiment_file_path"])
 
-    def __init__(self,config: Configuartion = Configuartion()) -> None:
+class Pipeline(Thread):
+    
+    experiment:Experiment=Experiment(*([None]*9))
+
+    def __new__(cls, *args,**kwargs):
+        if Pipeline.experiment.running_status:
+            raise Exception("Pipeline is already running")
+        return super(Pipeline,cls).__new__(cls)
+
+    def __init__(self, config: Configuration = Configuration()) -> None:
         try:
-            self.config=config
-
-
+            super().__init__(daemon=False, name="pipeline")
+            self.config = config
         except Exception as e:
-            raise HousingException(e,sys) from e
+            raise HousingException(e, sys) from e
 
-    def start_data_ingestion(self)->DataIngestionArtifact:
+    def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
             data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
-            raise HousingException(e,sys) from e    
+            raise HousingException(e, sys) from e
 
-
-    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact) \
-        -> DataValidationArtifact :
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) \
+            -> DataValidationArtifact:
         try:
-            data_validation =  DataValidation(data_validation_config=self.config.get_data_validation_config(),
-                                              data_ingestion_artifact=data_ingestion_artifact
-            )
+            data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                             data_ingestion_artifact=data_ingestion_artifact
+                                             )
             return data_validation.initiate_data_validation()
         except Exception as e:
-            raise HousingException(e,sys) from e
+            raise HousingException(e, sys) from e
 
     def start_data_transformation(self,
-                                data_ingestion_artifact:DataIngestionArtifact,
-                                data_validation_artifact: DataValidationArtifact
-                                )->DataTransformationArtifact:
+                                  data_ingestion_artifact: DataIngestionArtifact,
+                                  data_validation_artifact: DataValidationArtifact
+                                  ) -> DataTransformationArtifact:
         try:
             data_transformation = DataTransformation(
                 data_transformation_config=self.config.get_data_transformation_config(),
@@ -49,10 +68,16 @@ class Pipeline:
             )
             return data_transformation.initiate_data_transformation()
         except Exception as e:
-            raise HousingException(e,sys)
+            raise HousingException(e, sys)
 
-    def start_model_trainer(self):
-        pass
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
+            data_transformation_artifact=data_transformation_artifact
+            )
+            return model_trainer.initiate_model_trainer()
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
     def start_model_evaluation(self):
         pass
@@ -66,10 +91,12 @@ class Pipeline:
 
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            data_transformation_artifact = self.start_data_transformation(
-                data_ingestion_artifact=data_ingestion_artifact,
-                data_validation_artifact=data_validation_artifact
-            )
+            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                          data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact =self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_evaluation_artifact =self.start_model_evaluation(model_trainer_artifact=model_trainer_artifact)
+            
+            
             
 
 
